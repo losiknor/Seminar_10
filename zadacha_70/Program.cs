5a38ee83bbe9cc83e7b8ceeb47e6192ec4cd9b9a
Larina <Losiknor@gmail.com>
2022-03-11 14:42:06 +0700

    Решение задачи 70

--- zadacha_70/Program.cs
+++ zadacha_70/Program.cs
@@ -2,4 +2,19 @@
 // первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
 // 3 и 4, N = 5 -> 3 4 7 11 18
 // 6 и 10, N = 4 -> 6 10 16 26
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Задайте первое число:");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте второе число:");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Задайте количество чисел, которое нужно вывести:");
+int n = int.Parse(Console.ReadLine());
+int Row(int n, int a, int b)
+{
+    if (n == 1) return a;
+    if (n == 2) return b;
+    {
+        return Row(n-1, a, b) + Row(n-2, a, b);         
+    }
+}
+for (int i = 1; i <= n; i++)
+Console.Write(Row(i, a, b) + " ");
