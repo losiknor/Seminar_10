ad4971a54020f20c550f14df31e4ffe915947a2f
Larina <Losiknor@gmail.com>
2022-03-14 19:40:35 +0700

    Решение задачи 71

--- zadacha_71/Program.cs
+++ zadacha_71/Program.cs
@@ -3,5 +3,28 @@
 // букв этого алфавита.
 // n = 2 -> аа, ии, сс, вв, аи, иа, ис, си, ас, са,
 // ав, ва, ви, ив, св, вс
-
-Console.WriteLine("Hello, World!");
+string alf = "аисв";
+int n = 3;
+string[] Words(string a, int num)
+{
+    if (num == 0)
+    {
+        string[] resul = { "" };
+        return resul;
+    }
+    string[] w = Words(a, num - 1);
+    string[] result = new string[w.Length * 4];
+    int k = 0;
+    for (int i = 0; i < w.Length; i++)
+    {
+        for (int j = 0; j < a.Length; j++)
+        {
+            result[k] = w[i] + a[j];
+            k++;
+        }
+    }
+    return result;
+}
+string[] res = Words(alf, n);
+for (int i = 0; i < res.Length; i++)
+    Console.Write(res[i] + " ");
